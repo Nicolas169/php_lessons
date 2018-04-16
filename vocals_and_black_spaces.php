30ee6c53509978b77703c915398c7d8840147372
Nicolas169 <nicolas.dicaterina@gmail.com>
2018-04-16 15:55:32 -0300

    4/16/2018
    # Added HTML

--- vocals_and_black_spaces.php
+++ vocals_and_black_spaces.php
@@ -1,26 +1,52 @@
 <?php
-	// Given a string, delete all vocals, and show the result
-	$words = "hello world!";
+	// Nicolas Di Caterina
+	// Version: 1.0
+	// 4/16/18
+?>
 
-	// For ends by $words length
-	for ($i=0; $i < strlen($words); $i++) {
-		// If the char is a vocal, replace it by a blank char, and rest $i by 1, to mantain correct order
-		if (substr($words, $i, 1) == 'a' || substr($words, $i, 1) == 'e' || substr($words, $i, 1) == 'i' || substr($words, $i, 1) == 'o' || substr($words, $i, 1) == 'u') {
-			$words = substr_replace($words, '', $i, 1);
-			$i=-1;
-		}
-	}
-	// Show $word without vocals
-	echo $words."<br>";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vocals and blank spaces</title>
+</head>
+<body>
+	<form method="GET">
+		<input type="text" name="vocals" title="Words to delete all vocals" placeholder="Input words"><br>
+		<br>
+		<input type="text" name="blank_spaces" title="Words to delete all blank spaces" placeholder="Input words"><br>
+		<br>
+		<button>SUBMIT</button><br>
 
-	// Delete all blank spaces on a String
-	$words = "hello world, how are you?";
+		<?php
+			error_reporting(E_ERROR | E_WARNING | E_PARSE);
+			// Given a string, delete all vocals, and show the result
+			$words = $_GET['vocals'];
 
-	for ($i=0; $i < strlen($words); $i++) { 
-		if (substr($words, $i, 1) == ' ') {
-			$words = substr_replace($words, '', $i, 1);
-		}
-	}
+			// For ends by $words length
+			for ($i=0; $i < strlen($words); $i++) {
+				// If the char is a vocal, replace it by a blank char, and rest $i by 1, to mantain correct order
+				if (substr($words, $i, 1) == 'a' || substr($words, $i, 1) == 'e' || substr($words, $i, 1) == 'i' || substr($words, $i, 1) == 'o' || substr($words, $i, 1) == 'u') {
+				$words = substr_replace($words, '', $i, 1);
+				$i=-1;
+				}
+			}
+			// Show $word without vocals
+			echo $words."<br>";
 
-	echo $words;
+			// Delete all blank spaces on a String
+			$words = $_GET['blank_spaces'];
+
+			for ($i=0; $i < strlen($words); $i++) { 
+				if (substr($words, $i, 1) == ' ') {
+					$words = substr_replace($words, '', $i, 1);
+				}
+			}
+
+			echo $words;
+		?>
+	</form>
+</body>
+</html>
+<?php 
+
 ?>
